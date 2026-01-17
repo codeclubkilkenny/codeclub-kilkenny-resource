8943140506f95bc8738067b9a6bea738be95ad8f
Code Club Kilkenny <kilkenny.ie@coderdojo.com>
2026-01-17 00:00:55 +0000

    Refactor LED control to use Modulino library

--- arduino/arduinounor4/led-array/code.ino
+++ arduino/arduinounor4/led-array/code.ino
@@ -1,28 +1,43 @@
-// LED Array – simple chase pattern
-// Adjust the pins to match how your LED array is wired
+#include <Modulino.h>
 
-int leds[] = {2, 3, 4, 5, 6, 7, 8, 9};
-int ledCount = 8;
+// “LED Array” in the Plug and Make kit = Modulino Pixels (8 RGB LEDs)
+ModulinoPixels leds;
+
+const int BRIGHTNESS = 10; // % (they’re bright)
 
 void setup() {
-  // Set all LED pins as outputs
-  for (int i = 0; i < ledCount; i++) {
-    pinMode(leds[i], OUTPUT);
-  }
+  Serial.begin(115200);
+
+  // Start the Modulino I2C system, then the Pixels module
+  Modulino.begin();
+  leds.begin();
+
+  // Clear everything once at boot
+  leds.clear();
+  leds.show();
 }
 
 void loop() {
-  // Turn LEDs on one by one
-  for (int i = 0; i < ledCount; i++) {
-    digitalWrite(leds[i], HIGH);
-    delay(200);
-    digitalWrite(leds[i], LOW);
-  }
+  // A few built-in colours from the library
+  const ModulinoColor colors[] = { RED, GREEN, BLUE, VIOLET, WHITE };
+  const int nColors = sizeof(colors) / sizeof(colors[0]);
 
-  // And back the other way
-  for (int i = ledCount - 1; i >= 0; i--) {
-    digitalWrite(leds[i], HIGH);
-    delay(200);
-    digitalWrite(leds[i], LOW);
+  for (int c = 0; c < nColors; c++) {
+    // “Knight Rider” scan left -> right
+    for (int i = 0; i < 8; i++) {
+      leds.clear();
+      leds.set(i, colors[c], BRIGHTNESS);
+      leds.show();
+      delay(120);
+    }
+
+    // …and back right -> left
+    for (int i = 6; i >= 1; i--) {
+      leds.clear();
+      leds.set(i, colors[c], BRIGHTNESS);
+      leds.show();
+      delay(120);
+    }
   }
 }
+
