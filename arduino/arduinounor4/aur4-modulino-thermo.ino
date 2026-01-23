bc8924b0d3c232e47e390116ec623176d37de7d1
Code Club Kilkenny <kilkenny.ie@coderdojo.com>
2026-01-23 16:39:12 +0000

    Enhance comments for better code understanding
    Updated comments for clarity and added explanations for functions.

--- arduino/arduinounor4/aur4-modulino-thermo.ino
+++ arduino/arduinounor4/aur4-modulino-thermo.ino
@@ -1,31 +1,31 @@
-#include "Modulino.h"
-#include "ArduinoGraphics.h"
-#include "Arduino_LED_Matrix.h"
+#include "Modulino.h"                 // Gives us easy control of all Modulino sensors
+#include "ArduinoGraphics.h"          // Lets us draw text and shapes
+#include "Arduino_LED_Matrix.h"       // Controls the little LED screen on the Arduino
 
-ModulinoThermo thermo;
-ArduinoLEDMatrix matrix;
+ModulinoThermo thermo;                // Create the temperature sensor
+ArduinoLEDMatrix matrix;              // Create the LED matrix screen
 
-void showScroll(const char* msg) {
-  matrix.beginDraw();
-  matrix.stroke(0xFFFFFFFF);
-  matrix.textFont(Font_5x7);
-  matrix.textScrollSpeed(70);
-  matrix.beginText(0, 1, 0xFFFFFF);
-  matrix.println(msg);
-  matrix.endText(SCROLL_LEFT);
-  matrix.endDraw();
+void showScroll(const char* msg) {    // This function shows scrolling text on the screen
+  matrix.beginDraw();                 // Start drawing on the LED screen
+  matrix.stroke(0xFFFFFFFF);          // Turn LEDs on in white
+  matrix.textFont(Font_5x7);          // Choose a simple readable font
+  matrix.textScrollSpeed(70);         // Set how fast the text moves
+  matrix.beginText(0, 1, 0xFFFFFF);   // Start writing text at this position
+  matrix.println(msg);                // Write the message
+  matrix.endText(SCROLL_LEFT);        // Make the text scroll to the left
+  matrix.endDraw();                   // Show everything on the screen
 }
-
 void setup() {
-  matrix.begin();
-  Modulino.begin();
-  thermo.begin();
+  matrix.begin();                     // Turn on the LED matrix
+  Modulino.begin();                   // Turn on the Modulino system
+  thermo.begin();                     // Turn on the temperature sensor
 }
-
 void loop() {
-  float t = thermo.getTemperature();
-  char msg[16];
-  snprintf(msg, sizeof(msg), "%.1fC ", t);
-  showScroll(msg);
-  delay(250);
+  float t = thermo.getTemperature();  // Read the temperature in degrees Celsius
+
+  char msg[16];                       // Make a space to store the message
+  snprintf(msg, sizeof(msg), "%.1fC ", t); // Turn the number into text like 21.4C
+
+  showScroll(msg);                    // Show the temperature on the screen
+  delay(250);                         // Wait a short time before updating again
 }
